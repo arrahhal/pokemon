6eaa18083db6253bbac4216131376b3b59d3801f
arrahhal <arrahhal@proton.me>
2024-08-11 21:52:46 +0300

    initial implementation of Card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
 import Card from "./components/Card"
 
 function App() {
+  const [isFlipped, setFlip] = useState(false);
+  const [imgUrl, setImgUrl] = useState("/src/assets/pokeball.png");
+  const handleCardClick = () => {
+    setFlip(true);
+  };
+
+  const handleCardFlip = () => {
+    setFlip(false);
+    setImgUrl("/src/assets/music-off.png")
+  }
+
   return (
     <>
       <div>
-        <Card />
+        <Card name="pokemon" imgUrl={imgUrl} isFlipped={isFlipped} handleClick={handleCardClick} handleFlip={handleCardFlip} />
       </div>
     </>
   )
---
+++ src/components/Card.tsx
@@ -0,0 +1,28 @@
+interface CardProps {
+  name: string;
+  imgUrl: string;
+  isFlipped: boolean;
+  handleClick: () => void;
+  handleFlip: () => void;
+}
+
+export default function Card({ name = "pokemon name", imgUrl = "/src/assets/pokeball.png", isFlipped, handleClick, handleFlip }: CardProps) {
+  if (isFlipped) {
+    setTimeout(handleFlip, 1000);
+  }
+  return (
+    <div style={{ perspective: "1000px" }} className="w-44 aspect-[10/14] rounded" onClick={handleClick}>
+      <div style={{ transformStyle: "preserve-3d", transform: `rotateY(${isFlipped ? "180deg" : "0"})`, transition: "transform 0.8s" }} className="relative w-full h-full">
+        <div style={{ backfaceVisibility: "hidden" }} className="absolute w-full h-full flex flex-col bg-yellow-50">
+          <div className="flex-1 flex justify-center items-center">
+            <img src={imgUrl} width={"96px"} height={"96px"} />
+          </div>
+          <p className="font-bold text-xl text-center text-black bg-yellow-300 py-1">{name}</p>
+        </div>
+        <div style={{ transform: "rotateY(180deg)", transition: "transform 0.8s", backfaceVisibility: "hidden" }} className="absolute h-full w-full">
+          <img src="/src/assets/card-back.png" />
+        </div>
+      </div>
+    </div >
+  )
+}
