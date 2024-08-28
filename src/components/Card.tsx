d6c6b7720d49cbda7630f58859dd839f44fe0630
arrahhal <arrahhal@proton.me>
2024-08-28 22:38:10 +0300

    Style the Card a bit

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,15 +9,15 @@ interface CardProps {
 
 export default function Card({ name = "pokemon name", imgUrl = "/src/assets/pokeball.png", cardState = "front", handleClick, onTransitionEnd, id }: CardProps) {
   return (
-    <div style={{ perspective: "1000px" }} className="w-44 aspect-[10/14] rounded" onClick={handleClick} id={id.toString()}>
+    <div style={{ perspective: "1000px" }} className="w-44 aspect-[10/14] rounded justify-self-center" onClick={handleClick} id={id.toString()}>
       <div style={{ transformStyle: "preserve-3d", transform: `rotateY(${(cardState === "frontToBack" || cardState === "back") ? "180deg" : "0"})`, transition: "transform 0.8s" }} className="relative w-full h-full" onTransitionEnd={onTransitionEnd}>
-        <div style={{ backfaceVisibility: "hidden" }} className="absolute w-full h-full flex flex-col bg-yellow-50">
+        <div style={{ backfaceVisibility: "hidden" }} className="absolute w-full h-full flex flex-col bg-black/50 rounded-md">
           <div className="flex-1 flex justify-center items-center">
             <img src={imgUrl} width={"96px"} height={"96px"} />
           </div>
-          <p className="font-bold text-xl text-center text-black bg-yellow-300 py-1">{name}</p>
+          <p className="font-medium text-xl text-center text-white py-1">{name}</p>
         </div>
-        <div style={{ transform: "rotateY(180deg)", transition: "transform 0.8s", backfaceVisibility: "hidden" }} className="absolute h-full w-full">
+        <div style={{ transform: "rotateY(180deg)", transition: "transform 0.8s", backfaceVisibility: "hidden" }} className="absolute h-full w-full rounded-md">
           <img src="/src/assets/card-back.png" />
         </div>
       </div>
