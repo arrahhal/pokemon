7104461bc6c55192bfd3f9c1e26a400327eec312
arrahhal <arrahhal@proton.me>
2024-08-29 13:59:48 +0300

    adjust spinner styles

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,7 +1,10 @@
 export default function Spinner() {
   return (
     <div className={`absolute z-10 w-full h-full bg-white/10 backdrop-blur-sm flex justify-center items-center`}>
-      <img className="w-32 h-32 animate-spin" src="/src/assets/pokeball.svg" alt="pokeball" />
+      <div>
+        <img className="w-32 h-32 animate-spin mx-auto" src="/src/assets/pokeball.svg" alt="pokeball" />
+        <p className="bg-white/30 px-4 py-2 rounded-xl font-medium text-black/60">Loading Pokemons...</p>
+      </div>
     </div>
   )
 }
