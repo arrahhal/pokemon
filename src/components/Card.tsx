ae54ccb40435e663beb407d222dcc7f824c8a2d8
arrahhal <arrahhal@proton.me>
2024-08-29 14:27:13 +0300

    adjust font

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -15,7 +15,7 @@ export default function Card({ name = "pokemon name", imgUrl = "/src/assets/poke
           <div className="flex-1 flex justify-center items-center">
             <img src={imgUrl} width={"96px"} height={"96px"} />
           </div>
-          <p className="font-medium text-xl text-center text-white py-1">{name}</p>
+          <p className="text-center text-white py-1">{name}</p>
         </div>
         <div style={{ transform: "rotateY(180deg)", transition: "transform 0.8s", backfaceVisibility: "hidden" }} className="absolute h-full w-full rounded-md">
           <img src="/src/assets/card-back.png" />
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,6 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
-    fontFamily: {
-      pokemon: ['"Press Start 2P"', '"system-ui"'],
-    },
   },
   plugins: [],
 };
